4005c3776f4cc7788877de167530efebec12733c
tmaksymovych <tmaksymovych@kse.org.ua>
2025-06-08 20:27:05 +0300

    1-Filter by Multiple Conditions.py

--- assignmen#3/assignment-3/3-medium_level/10-Multiply If.py
+++ assignmen#3/assignment-3/3-medium_level/10-Multiply If.py
@@ -4,7 +4,7 @@ numbers_list = [random.randint(-8096, 15730) for _ in range(124)]
 print("numbers:", numbers_list)
 
 Y = int(input("Enter multyplier:"))
-X = int(input("Enter limiter:/n"))
+X = int(input("Enter limiter:"))
 
 multyplied_list = [nums for nums in numbers_list * Y if nums > X]
 print("Multiplied list:", multyplied_list)
